250f9e4c6439286c0aa45ac643e6fbb4c76941ea
sbalasz <41318140+sbalasz@users.noreply.github.com>
2024-06-20 22:03:25 +0100

    Update check-module.ps1

--- scripts/infrastructure/check-module.ps1
+++ scripts/infrastructure/check-module.ps1
@@ -1,4 +1,5 @@
 #Script to check Exel module existence on device
+#If doesn not exist on device, it will then get installed
  $module_name = 'importExcel'
 if(-not (Get-Module $module_name -ListAvailable)){
 Install-Module $module_name -Scope CurrentUser -Force
