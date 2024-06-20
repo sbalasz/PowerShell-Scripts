c33a89a7d410c753fc5e330900ae1a5333c86977
sbalasz <41318140+sbalasz@users.noreply.github.com>
2024-06-20 22:04:50 +0100

    Update convert-exelToCsv.ps1

--- scripts/1st-line/convert-exelToCsv.ps1
+++ scripts/1st-line/convert-exelToCsv.ps1
@@ -1,11 +1,14 @@
-#Script to convert excel to csv
+# This script converts Excel files to CSV format
+param (
+    # Path to the Excel file
+    [string]$excelFilePath,
+    
+    # Path to save the CSV file
+    [string]$csvFilePath
+)
 
-#HR file path
- $hrFilePath = "\\share\filepath"
-#Checking if csv in place, if not, it will convert excel
-if (!(Test-Path -Path "$hrFilePath\$today.csv")){
-    Write-host("Converting excel file to csv... ") -BackgroundColor Blue
-    $hrExport = Import-Excel -Path "$hrFilePath\$today.xlsx" -StartRow 7
-    $convertedHRreport = $hrExport | Export-Csv -Encoding UTF8 "$hrFilePath\$today.csv" -NoTypeInformation
-    Write-host("Excel converted to csv and saved to $hrFilePath") -BackgroundColor Blue
-}else{Write-host("CSV for today already in place") -BackgroundColor Green}
+# Import the Excel file
+$excel = Import-Excel -Path $excelFilePath
+
+# Export the data to CSV
+$excel | Export-Csv -Path $csvFilePath -NoTypeInformation
